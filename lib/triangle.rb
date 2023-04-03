a0f77c110935b4ff65b8309f906433ab85d3af14
Tate <tatebraeckel@gmail.com>
2023-04-03 13:09:37 -0600

    commit tree 391b65f305f422b7071d8a90ef0358d87ff93870

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+attr_accessor :a, :b, :c
+
+def initialize(a,b,c)
+  @a = a 
+  @b = b
+  @c = c
+end
+
+def kind 
+  if a ==b && b = c 
+    :equilateral
+  elseif a == b || b == c || a == c 
+  :isosceles
+  else 
+  :scalene
+  end
+end
+
+ 
+
+
+#   class TriangleError < StandardError
+
+#   end
+# end
+
 end
